00df02d511dfa89ccbf00771aee7632a49bb98b7
Aliss1902 <alissodayanamejia@gmail.com>
2025-09-19 15:32:20 -0600

    implementacion de mongodb

---
+++ db.js
@@ -0,0 +1,10 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+process.env.MONGODB_URI;
+
+
+
+
+function PalabrasClave(params) {
+    
+}
---
+++ deteccion.js
@@ -0,0 +1,3 @@
+function Comparacion(params) {
+    const PalabraClave= []
+}
