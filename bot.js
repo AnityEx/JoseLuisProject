5f97419715c57fd93386f3d262b003182b121ee9
AnityEx <61922421+AnityEx@users.noreply.github.com>
2025-09-23 10:16:18 -0600

    bot cosas edit

--- bot.js
+++ bot.js
@@ -1,30 +1,38 @@
 const TelegramBot = require('node-telegram-bot-api');
-const fs = require('fs');  // <-- Aquí importas fs
-
-// 🔑 Pega aquí tu token
+const fs = require('fs');
 const token = '8305739458:AAHzOd_jbPr8gvzZ3kovxoQspXmpoSZWnSU';
 
-// ⚙️ Crea el bot (modo polling: escucha mensajes)
 const bot = new TelegramBot(token, { polling: true });
 
-// 🤖 Cuando alguien escribe /start
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, '¡Hola! Soy tu bot de Telegram 🤖');
+    bot.sendMessage(chatId, 'Sospechas de algun fraude? puedo ayudarte!');
 });
 
-// 📝 Cuando recibe cualquier otro mensaje
+
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
-    
-    if (text === '/secret') {
-        bot.sendMessage(chatId, `alav conoces la llave 👀`);
-/*         bot.sendDocument(chatId, fs.createReadStream('pdf/Proyect002.pdf'));
- */    }
-    else if (text !== '/start') {
-        bot.sendMessage(chatId, `Dijiste: "${text}" 👀`);
-/*         bot.sendPhoto(chatId, fs.createReadStream('img/kirbo.jpg')); */
+
+    switch (text) {
+        case '/start':
+            // No hacer nada
+            break;
+        case '/info':
+            bot.sendMessage(chatId, `/info\nLista de comandos:\n---------------------------------------------------------------\n/texto - Revisaré mensajes sospechosos \n/captura - Envia una imagen y lo analizaré`);
+            break;
+        case '/texto':
+            bot.sendMessage(chatId, `Necesitas ayuda? Pon el mensaje de texto que te han enviado en el siguiente chat`);
+            break;
+        case '/captura':
+            bot.sendMessage(chatId, `Necesitas ayuda? Pon la captura que sobre lo que te han enviado en el siguiente chat`)
+            break;
+        default:
+            bot.sendMessage(chatId, `"${text}" no es un comando, usa /info para la lista de comandos`);
+
+            break;
     }
+
+
 });
